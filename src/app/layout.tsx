3faeb15ae98a7af3f93cd1520874a00729849f12
Rion <pien@piennu777.jp>
2024-12-01 12:06:05 +0900

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -7,41 +7,45 @@ import zisty_header from "@/assets/Images/zisty_header.png"
 import '@/assets/CSS/element.css'
 import '@/assets/CSS/header.css'
 
-const url = "https://zisty.net/";
-const favicon = "/favicon.png";
-const ogpIcon = "/ogp.webp";
-const siteName = "Zisty";
-const description = "Zisty is a team that was randomly formed by saying, “Come join us if you want to! Probably, or certainly, the best team in the universe (I'm sure).";
-const metadata = {
-  title: "Zisty",
-  description: "Zisty is a team that was randomly formed by saying, “Come join us if you want to! Probably, or certainly, the best team in the universe (I'm sure).",
-  openGraph: {
-    title: siteName,
-    description,
-    url,
-    siteName,
-    locale: "ja-JP",
-    type: "website",
-    images: ogpIcon
-  },
-  icons: favicon,
-  verification: {
-    google: ""
-  },
-  publisher: `@teamzisty`,
-  robots: "index, follow",
-  creator: `@teamzisty`,
-  keywords: ['Zisty', 'TeamZisty', 'ZISTY', 'teamzisty'],
+export const metadata = {
+	title: {
+		default: 'Zisty',
+		template: '%s / Zisty'
+	},
+	description: 'We are a team of engineers with the slogan “We turn the unreal to real”.',
+	metadataBase: new URL('https://zisty.net'),
+	openGraph: {
+		title: 'We turn the unreal to real',
+		description: 'We are a team of engineers with the slogan “We turn the unreal to real”.',
+		url: 'https://zisty.net',
+		siteName: 'Zisty',
+		images: [
+			{
+				url: '/ogp.webp',
+				width: 1200,
+				height: 630,
+			}
+		],
+		locale: 'ja_JP',
+		type: 'website',
+	},
+	icons: {
+		icon: '/favicon.png',
+	},
+	robots: {
+		index: true,
+		follow: true,
+	}
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="ja">
 			<body>
-			<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
-			<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
+				<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" />
+				<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" />
 
-			<div className="header">
+				<div className="header">
 					<div className="left-links">
 						<a className="header-a" href="https://zisty.net/">
 							<Image src={zisty_header} width={18} height={18} alt="Zisty Icon" />
